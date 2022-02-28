796324a721668bc86073308acd5342cd6568c90e
solairerove <solairerove@gmail.com>
2022-03-01 01:17:44 +0300

    feature: write async test as well

--- src/test/kotlin/com/github/solairerove/amzscouttesttask/ApplicationTest.kt
+++ src/test/kotlin/com/github/solairerove/amzscouttesttask/ApplicationTest.kt
@@ -1,19 +1,18 @@
 package com.github.solairerove.amzscouttesttask
 
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.runBlocking
 import org.hamcrest.CoreMatchers.containsString
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.ApplicationContext
-import org.springframework.mock.http.server.reactive.MockServerHttpRequest.get
-import org.springframework.mock.web.MockHttpServletRequest
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import org.springframework.test.web.servlet.setup.MockMvcBuilders
-import org.springframework.web.context.WebApplicationContext
 
 // вообще я использую котест, но спринг инишилайзер добавил жюнит, поэтому вот
 @AutoConfigureMockMvc
@@ -54,5 +53,35 @@ class ApplicationTest {
         mockMvc.get("/api/v1/limitation") {
             header("Host", "bad-gateway")
         }.andExpect { status { status().isBadGateway } }
+    }
+
+    @Test
+    fun shouldReturn502StatusAsync() {
+        runBlocking {
+            coroutineScope {
+                awaitAll(
+                    async {
+                        mockMvc.get("/api/v1/limitation") {
+                            header("Host", "bad-gateway-async")
+                        }.andExpect { status { status().isOk } }
+                    },
+                    async {
+                        mockMvc.get("/api/v1/limitation") {
+                            header("Host", "bad-gateway-async")
+                        }.andExpect { status { status().isOk } }
+                    },
+                    async {
+                        mockMvc.get("/api/v1/limitation") {
+                            header("Host", "bad-gateway-async")
+                        }.andExpect { status { status().isOk } }
+                    },
+                    async {
+                        mockMvc.get("/api/v1/limitation") {
+                            header("Host", "bad-gateway-async")
+                        }.andExpect { status { status().isBadGateway } }
+                    }
+                )
+            }
+        }
     }
 }
